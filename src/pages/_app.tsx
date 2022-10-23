79667bb441b93f4fcdc9dc15c514db3b7a46a140
lmw <alsdndjdrktl@gmail.com>
2022-10-23 19:01:45 +0900

    feat(#21): Header 컴포넌트 구현
    - 헤더 컴포넌트
    - 스크롤시 헤더 숨김 처리

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import '~/styles/globals.scss'
 import '~/styles/tailwind.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import Header from '~/components/organism/header/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
- return <Component {...pageProps} />
+ return (
+  <ChakraProvider>
+   <Header />
+   <Component {...pageProps} />
+  </ChakraProvider>
+ )
 }
 
 export default MyApp
