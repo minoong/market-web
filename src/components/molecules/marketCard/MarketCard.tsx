248c230e7ba6a5680d969c9f2f62a99cc625aff2
lmw <alsdndjdrktl@gmail.com>
2022-11-08 21:18:45 +0900

    refactor(#32): 마켓 카드 캐러셀 리팩터링
    - selector로 메모제이션

--- src/components/molecules/marketCard/MarketCard.tsx
+++ src/components/molecules/marketCard/MarketCard.tsx
@@ -5,14 +5,16 @@ import { MarketTicker } from '~/interface/upbit/market/ticker'
 import styles from '~/components/molecules/marketCard/MarketCard.module.scss'
 import Image from 'next/image'
 import Candle from '~/components/atoms/candle/Candle'
+import { marketKrwSymbolSelector, marketSymbolSelector } from '~/features/upblit/market/symbolSlice'
+import { shallowEqual } from 'react-redux'
+import { MarketSymbol } from '~/interface/upbit/market/marketSymbol'
 
 interface Props {
- logo: string
  marketSymbol: string
 }
 
 function MarketCard(props: Props) {
- const { logo, marketSymbol } = props
+ const { marketSymbol } = props
 
  const marketsSymbol = useAppSelector((state) =>
   state.marketSymbol.symbol
@@ -20,59 +22,55 @@ function MarketCard(props: Props) {
    .map(({ market }) => market)
    .join(','),
  )
- const marketsKorSymbol = useAppSelector((state) => state.marketSymbol.symbol)
+ const { korean_name: koreanName } = useAppSelector(
+  (state) => marketSymbolSelector(state, marketSymbol),
+  shallowEqual,
+ ) as MarketSymbol
+ const splitSymbol = useAppSelector((state) => marketKrwSymbolSelector(state, marketSymbol), shallowEqual)
 
  const queryCleint = useQueryClient()
- const dataList = queryCleint
+ const marketTicker = queryCleint
   .getQueryData<MarketTicker[]>(['markets', marketsSymbol])
   ?.find((d) => d.market === marketSymbol) as MarketTicker
 
- const enName = useMemo(() => {
-  const [krw, symbol] = dataList.market.split('-')
-
-  return `${symbol}/${krw}`
- }, [dataList])
-
- console.log(styles[dataList.change])
-
  return (
-  <div className={`${styles['market-card-wrapper']} ${styles[dataList.change]}`}>
-   {dataList && (
+  <div className={`${styles['market-card-wrapper']} ${styles[marketTicker.change]}`}>
+   {marketTicker && (
     <>
      <div>
       <Image
-       src={`https://static.upbit.com/logos/${dataList.market.split('-')[1]}.png`}
+       src={`https://static.upbit.com/logos/${marketTicker.market.split('-')[1]}.png`}
        width={26 * 1.2}
        height={26 * 1.2}
-       alt={dataList.market}
+       alt={marketTicker.market}
       />
      </div>
 
      <div className={styles['market-symbol-wrapper']}>
-      <div>{marketsKorSymbol.find((d) => d.market === dataList.market)?.korean_name}</div>
-      <div>{enName}</div>
+      <div>{koreanName}</div>
+      <div>{splitSymbol}</div>
      </div>
      <div className={styles['market-symbol-current-price']}>
-      <div>{dataList.trade_price.toLocaleString()}</div>
+      <div>{marketTicker.trade_price.toLocaleString()}</div>
      </div>
-     <div className={styles['market-symbol-wrapper']}>
-      <div>{dataList.signed_change_price.toLocaleString()}</div>
+     <div className={styles['market-symbol-rate-wrapper']}>
+      <div>{marketTicker.signed_change_price.toLocaleString()}</div>
       <div>
-       {dataList.signed_change_rate > 0 ? '+' : ''}
-       {(dataList.signed_change_rate * 100).toFixed(2)}%
+       {marketTicker.signed_change_rate > 0 ? '+' : ''}
+       {(marketTicker.signed_change_rate * 100).toFixed(2)}%
       </div>
      </div>
      <div className={styles['market-candle-wrapper']}>
       <svg width={15} height={26 * 1.2} fill="black">
        <Candle
-        fill={dataList.change === 'RISE' ? '#c84a31' : '#1261c4'}
+        fill={marketTicker.change === 'RISE' ? '#c84a31' : '#1261c4'}
         width={15}
         height={26 * 1.2}
         price={{
-         openingPrice: dataList.opening_price,
-         tradePrice: dataList.trade_price,
-         highPrice: dataList.high_price,
-         lowPrice: dataList.low_price,
+         openingPrice: marketTicker.opening_price,
+         tradePrice: marketTicker.trade_price,
+         highPrice: marketTicker.high_price,
+         lowPrice: marketTicker.low_price,
         }}
        />
       </svg>
--- src/features/upblit/market/symbolSlice.ts
+++ src/features/upblit/market/symbolSlice.ts
@@ -1,4 +1,5 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { RootState } from '~/features/store'
 import { MarketSymbol } from '~/interface/upbit/market/marketSymbol'
 
 export interface SymbolState {
@@ -17,6 +18,19 @@ export const symbolSlice = createSlice({
    state.symbol = action.payload
   },
  },
+})
+
+const selectMarketsSymbol = (state: RootState) => state.marketSymbol.symbol
+
+export const marketSymbolSelector = createSelector(
+ [selectMarketsSymbol, (_, marketSymbol: string) => marketSymbol],
+ (marketsSymbol, marketSymbol) => marketsSymbol.find((market) => market.market === marketSymbol) as MarketSymbol,
+)
+
+export const marketKrwSymbolSelector = createSelector([marketSymbolSelector], (marketSymbol) => {
+ const [krw, symbol] = marketSymbol.market.split('-')
+
+ return `${symbol}/${krw}`
 })
 
 export const { invoke } = symbolSlice.actions
